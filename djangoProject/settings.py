4678c1f181076a4cce04d926a977daedcb2f2cdb
hrr2000 <thedragonea@gmail.com>
2022-11-25 01:47:00 +0200

    chore: added user profile model

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'tags',
     'categories',
     'levels',
+    'contests'
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -1,5 +1,20 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
+from levels.models import Level
+from contests.models import Contest
 
 
 class User(AbstractUser):
     pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    codeforces_handle = models.CharField(max_length=255, blank=True)
+    level = models.ForeignKey(Level, on_delete=models.SET_NULL, null=True, blank=True)
+    contests_registered = models.ManyToManyField(Contest, blank=True)
+
+    def __str__(self):
+        return self.user.username
